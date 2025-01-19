e34ae7533306f7273033405dfd3d9e7076ab6bf9
kw-00 <christopher.p.wojcik@gmail.com>
2025-01-19 22:20:11 +0100

    prepare regressors for searching

--- utils.py
+++ utils.py
@@ -1,18 +1,19 @@
 from typing import Any, Dict, List, Tuple, TypedDict
 from sklearn.base import BaseEstimator
 from sklearn.model_selection import BaseCrossValidator, BaseShuffleSplit, GridSearchCV
-from sklearn.utils._typedefs import MatrixLike
 
+import pandas as pd
+import numpy as np
 
 
 def select_estimator(
         estimators_and_param_grids: List[Tuple[List[BaseEstimator], Dict[str, Any]]],
         cv: BaseCrossValidator | BaseShuffleSplit,
-        X: MatrixLike,
-        y: MatrixLike,
+        X: Any,
+        y: Any,
         scoring: str | None = None,
     ) \
-        -> BaseEstimator:
+        -> List[GridSearchCV]:
     
     grid_searches: List[GridSearchCV] = []
     for estimators, param_grid in estimators_and_param_grids:
@@ -28,8 +29,18 @@ def select_estimator(
     for gs in grid_searches:
         gs.fit(X=X, y=y)
 
-    best_search = max(grid_searches, key=lambda gs: gs.best_score_)
-    return best_search.best_estimator_
+    return grid_searches
+
+
+def remove_and_fill_non_numerical(df: pd.DataFrame, values_counting_as_na: List[str]) -> None:
+    for v in values_counting_as_na:
+        df.replace(v, np.nan, inplace=True)
+
+    for column_name in df.dropna().select_dtypes(include=["number"]):
+        df[column_name] = df[column_name].fillna(df.dropna()[column_name].mean())
+
+    for column_name in df.dropna().select_dtypes(exclude=["number"]):
+        df.drop(labels=[column_name], axis="columns", inplace=True)
 
     
         
